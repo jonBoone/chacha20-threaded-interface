c4068f63e6dd396c631d871812519d62a6f54ddc
Jon Boone <ipmonger@delamancha.org>
2020-12-31 18:38:13 -0500

    corrects script so that it passes shellcheck

--- test-changes.sh
+++ test-changes.sh
@@ -12,7 +12,7 @@ attempts=10
 # usage()
 #
 function usage {
-    echo "usage: `basename ${BASH_SOURCE[0]}` [-a num_attempts] [-f filename_to_transfer] [-s source_location] [-t target_location]"
+    echo usage: "$(basename "\"${BASH_SOURCE[0]}\"")" [-a num_attempts] [-f filename_to_transfer] [-s source_location] [-t target_location]
 }
 
 
@@ -32,11 +32,14 @@ done
 echo -e "attempts: ${attempts}\nfilename=${filename}\nsrcloc=${srcloc}\ntargetloc=${targetloc}\n"
 
 ## generate a random uuid for this run of the script
-uuid=`uuidgen`
+uuid=$(uuidgen)
 
-for attempt in `seq 1 ${attempts}`; do
-    ts_epoch=`epoch.py`
-    ./scp -S ./ssh -vvv ${srcloc}/${filename} ${targetloc}/${uuid}-${ts_epoch}-${filename} > /tmp/${uuid}-${ts_epoch}-debug.txt 2>&1
+for attempt in $(seq 1 "${attempts}"); do
+    ts_epoch=$(epoch.py)
+    targethost=$(echo "${targetloc}" | cut -d: -f 1)
+    targetdir=$(echo "${targetloc}" | cut -d: -f 2)
+    ./ssh "${targethost}" touch "${targetdir}"/"${uuid}"-"${ts_epoch}"-"${attempt}".txt
+    ./scp -S ./ssh -vvv "${srcloc}"/"${filename}" "${targetloc}"/"${uuid}"-"${ts_epoch}"-"${filename}" > /tmp/"${uuid}"-"${ts_epoch}"-debug.txt 2>&1-
 done
 
 # rm -f ~/ssh.c ./debug.txt && make && ./scp -S ./ssh ssh.c localhost:. > debug.txt 2>& 1 && diff ./ssh.c $HOME/ssh.c && shasum -a 256 ./ssh.c ~/ssh.c
